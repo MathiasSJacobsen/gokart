f09b9b1964442f3153d68c753dba347a7c7c00ea
Mathias S. Jacobsen <mathias.skallerud@gmail.com>
2024-03-30 18:07:41 +0100

    grid cols 1 on moblie landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Landing } from "@/components/Landing";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between m-20 mt-10">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:m-20 lg:mt-10 m-5">
       <Landing />
     </main>
   );
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -10,7 +10,7 @@ export const Landing = () => {
   return (
     <div className="w-full">
       <h1 className="font-bold text-xl mb-5">Alle runder</h1>
-      <div className="grid grid-cols-2 gap-7 w-full">
+      <div className="grid lg:grid-cols-2 gap-7 w-full">
         {Object.keys(data).map((e: string) => (
           <PersonalStat
             key={e}
